50b7e9586bf9c2c8d6cb487c60ad4fff514040b9
MUSABENDE-Michel <musabendemichel@gmail.com>
2025-11-20 09:56:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App(){
+return(
+  <>
+{/* usse tailwind */}
+  <p className="flex text-blue-500 p-5 border-8 justify-center font-bold">Hello World</p>
+  <p className="flex display-flex bg-green-300 justify-end">hello Michel</p>
+  </>
+
+);
+
+}
+export default App;
